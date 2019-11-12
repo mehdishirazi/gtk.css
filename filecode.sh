4eaea03c0db3ecd6fe9fe7196328d36554ffb806
Mehdi Shirazi <msh199271@gmail.com>
2019-11-12 05:35:51 -0500

    Added file code of gtk-3.0

---
+++ filecode.sh
@@ -0,0 +1,18 @@
+.terminator-terminal-window notebook stack{
+  border: 0px;
+}
+
+
+.terminator-terminal-window notebook header,
+.terminator-terminal-window notebook tab{
+  background: #000000;
+  border: 1px solid #303030;
+  padding: 0px;
+}
+
+.terminator-terminal-window notebook tab:checked {
+  background: #000000;
+  color: #008080;
+  border: 0px solid black;
+}
+
